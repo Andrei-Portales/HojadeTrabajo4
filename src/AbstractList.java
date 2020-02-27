d8312aa2ebadc06ff361ffc67e61fd6e4c140f41
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-02-26 21:54:15 -0600

    doc

--- src/AbstractList.java
+++ src/AbstractList.java
@@ -6,12 +6,18 @@ public abstract class AbstractList<E>
    {
    }
 
+   /**
+    * retorno si la lista esta vacio o no
+    */
    public boolean isEmpty()
    // post: returns true iff list has no elements
    {
       return size() == 0;
    }
   
+   /**
+    * retorna si el valor ingresado esta en la lista
+    */
   public boolean contains(E value)
   // pre: value is not null
   // post: returns true iff list contains an object equal to value
